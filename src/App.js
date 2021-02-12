b1f609347d0ccc4aebb12680eb658a4fadd6c8a0
mhsoun <m.mhsoon@gmail.com>
2021-02-12 16:03:15 +0100

    testing library

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
-import React, {useState, useEffect} from "react";
 import './App.css';
 
-//https://api.github.com/users/mhsoun
 
-
-function App({login}) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+function App() {
   
-  useEffect(()=> {
-    if(!login) return;
-    setLoading(true);
-    fetch(`https://api.github.com/users/${login}`)
-    .then((response)=> response.json())
-    .then(setData)
-    .then(()=> setLoading(false))
-    .catch(setError);
-  },[login]);
-
-  if(loading) return <h1> Loading...</h1>;
-  if(error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
-  if (!data) return null;    
-
   return (
   <div>
-    <h1>{data.name}</h1>
-    <p>{data.location}</p>
-    <img alt={data.login} src={data.avatar_url} height={200}/>
+    <h1>Hello react test library</h1>
   </div>
   );
 }
